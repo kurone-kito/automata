7361807cab67695cd247e4fb042f9ba9508a6796
kurone-kito <krone@kit.black>
2026-02-08 16:56:24 +0900

    feat: added the ja support to cloud-init configuration

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "multipass" {}
 
 resource "multipass_instance" "build" {
   cloudinit_file = "${path.module}/cloud-init.yml"
+  cpus           = 2
   image          = "resolute"
   name           = "automata"
 }
