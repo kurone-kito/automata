be56673891403d3d42f601c87c1873fb67aa369c
kurone-kito <krone@kit.black>
2026-02-21 14:27:33 +0900

    feat: added the build tools to the cloud-init configuration

--- lib/base-install.sh
+++ lib/base-install.sh
@@ -8,8 +8,8 @@ cd "$(cd "$(dirname "$0")"; pwd)/.."
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install -y --no-install-recommends ca-certificates curl \
-  gh gist git gnupg locales
+sudo apt-get install -y --no-install-recommends build-essential \
+  ca-certificates cmake curl g++ gh gist git gnupg locales make
 
 sudo apt-get autoremove -y
 sudo apt-get clean
