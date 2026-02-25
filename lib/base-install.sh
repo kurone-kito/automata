339b3bb40db6b5adc11c17b5381d34cdbea6d44f
kurone-kito <krone@kit.black>
2026-02-25 19:00:42 +0900

    feat: added the python installation

--- lib/base-install.sh
+++ lib/base-install.sh
@@ -9,7 +9,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install -y --no-install-recommends build-essential \
-  ca-certificates cmake curl g++ gh gist git gnupg locales make
+  ca-certificates cmake curl g++ gh gist git gnupg locales make python3 \
+  python3-pip
 
 sudo apt-get autoremove -y
 sudo apt-get clean
