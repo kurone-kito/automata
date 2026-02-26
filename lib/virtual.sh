f631a86008679cabde97a9cbe7d3cd96dc2e2b4d
kurone-kito <krone@kit.black>
2026-02-27 08:01:04 +0900

    feat: added the snapshot step after vm setup

--- lib/virtual.sh
+++ lib/virtual.sh
@@ -7,3 +7,8 @@ cd "$(cd "$(dirname "$0")"; pwd)/.."
 
 terraform init
 terraform apply -auto-approve
+
+VM='automata'
+multipass stop "${VM}"
+multipass snapshot -n vanilla "${VM}" || true
+multipass start "${VM}"
